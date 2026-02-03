c70178643b9c23041004053e94d7514634d7ae30
vanthuc1810 <vanthuc08888@gmail.com>
2026-02-03 21:54:05 +0700

    chore: remove redundace props

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -3,19 +3,16 @@
 import { useState } from "react";
 import { ChatInput } from "../components/chat/ChatInput";
 
-export const ChatPage = () => {
+const ChatPage = () => {
   const [value, setValue] = useState("");
 
   return (
     <>
-      {/* <MessageList /> */}
       <div className="p-4 border-t border-[#262A33]">
-        <ChatInput
-          value={value}
-          onChange={setValue}
-          onSend={() => console.log(value)}
-        />
+        <ChatInput onSend={() => console.log(value)} />
       </div>
     </>
   );
 };
+
+export default ChatPage;
